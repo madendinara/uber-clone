a62284c567d49e77e0deb3a267dd87840f05bd18
Dinara Zimanova <madendinara@gmail.com>
2021-12-17 18:25:32 -0500

    Displaying user location

--- uber-clone/Controller/HomeController.swift
+++ uber-clone/Controller/HomeController.swift
@@ -12,20 +12,30 @@ import MapKit
 class HomeController: UIViewController {
     
     // MARK: - Properties
-    private lazy var mapView = MKMapView()
+    private let mapView = MKMapView()
+    private let locationManager = CLLocationManager()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         checkIfUserLoggedIn()
 //        signOut()
+        enableLocationServices()
     }
     
     // MARK: - Methods
     func configureView() {
+        configureMapView()
+    }
+    
+    func configureMapView() {
         [mapView].forEach { view.addSubview($0) }
         makeConstaints()
-    }
+        
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        let region = MKCoordinateRegion(center: mapView.centerCoordinate, span: MKCoordinateSpan(latitudeDelta: mapView.region.span.latitudeDelta * 1.05, longitudeDelta: mapView.region.span.longitudeDelta * 1.05))
+        self.mapView.setRegion(region, animated: true)    }
     
     func makeConstaints() {
         mapView.snp.makeConstraints { make in
@@ -55,6 +65,32 @@ class HomeController: UIViewController {
             try Auth.auth().signOut()
         } catch let error {
             print("Error of signing out is \(error.localizedDescription)")
+        }
+    }
+}
+
+// MARK: - Location Services
+extension HomeController: CLLocationManagerDelegate {
+    func enableLocationServices () {
+        locationManager.delegate = self
+        switch CLLocationManager.authorizationStatus() {
+        case .restricted, .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways:
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        case .authorizedWhenInUse:
+            locationManager.requestAlwaysAuthorization()
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
         }
     }
 }
